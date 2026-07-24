324cb14214a62f2f5b268bb1d1f9962ed362615b
Robigo1977 <robigo.hun@gmail.com>
2026-07-24 15:32:11 +0100

    Complete Winchester carpet cleaning SEO page

--- src/pages/CarpetCleaningWinchester.tsx
+++ src/pages/CarpetCleaningWinchester.tsx
@@ -1,6 +1,8 @@
 import Hero from "../components/seo-pages/carpet-cleaning/Hero";
 import Benefits from "../components/seo-pages/carpet-cleaning/Benefits";
 import Process from "../components/seo-pages/carpet-cleaning/Process";
+import FAQ from "../components/seo-pages/carpet-cleaning/FAQ";
+import CTA from "../components/seo-pages/carpet-cleaning/CTA";
 
 export default function CarpetCleaningWinchester() {
   return (
@@ -8,6 +10,8 @@ export default function CarpetCleaningWinchester() {
       <Hero />
       <Benefits />
       <Process />
+      <FAQ />
+      <CTA />
     </main>
   );
 }
