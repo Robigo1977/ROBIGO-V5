9434e4e3fde5d3e65b09b0ae6f5cc076aa6d764c
Robigo1977 <robigo.hun@gmail.com>
2026-07-24 15:37:15 +0100

    Enable browser routing for SEO pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
@@ -9,7 +10,9 @@ import "./styles/globals.css";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HelmetProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </HelmetProvider>
   </StrictMode>
 );
