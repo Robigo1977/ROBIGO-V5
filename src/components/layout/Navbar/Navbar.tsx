a3118a1eff133a8f33bc290d15c8058d40f66b36
Robigo1977 <robigo.hun@gmail.com>
2026-08-08 13:54:26 +0100

    Remove all top navigation from homepage

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -7,21 +7,22 @@ import { site } from "../../../config/site";
 
 export default function Navbar() {
   const { pathname } = useLocation();
-  const isHome = pathname === "/";
+
+  if (pathname === "/") {
+    return null;
+  }
 
   return (
-    <header className={`navbar${isHome ? " navbar-home" : ""}`}>
+    <header className="navbar">
       <Container>
-        <div className={`navbar-container${isHome ? " navbar-container-home" : ""}`}>
-          {!isHome && (
-            <a
-              href="/"
-              className="navbar-logo"
-              aria-label={`${site.business.name} Home`}
-            >
-              {site.business.name}
-            </a>
-          )}
+        <div className="navbar-container">
+          <a
+            href="/"
+            className="navbar-logo"
+            aria-label={`${site.business.name} Home`}
+          >
+            {site.business.name}
+          </a>
 
           <nav
             className="navbar-nav"
